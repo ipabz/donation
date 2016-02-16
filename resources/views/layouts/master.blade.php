24311ec64dc907f9db2250080dd1d5c0bf690dd4
ljlerit <ljlerit@gmail.com>
2016-02-16 11:27:21 +0800

    added eric's template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
-<!--[if (lt IE 10)|(IE 10) ]><html lang="en" class="ie"><![endif]-->
-<!--[if (gt IE 10)|!(IE)]><!--> <html lang="en" class=""> <!--<![endif]-->
-	<head>
-		@include('layouts.common.head')
-	</head>
-	<body>
-		<!--[if lt IE 10 ]>
-			<div class="ienotice">
-				You are using an <strong>outdated</strong> browser. 
-				Please <a href="http://browsehappy.com/">upgrade your browser</a> 
-				or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> 
-				to improve your experience.
-			</div>
-		<![endif]-->
-		
-		@yield('content')
-		@include('layouts.common.head')
-	</body>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="text/html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Tulsa Community Foundation</title>
+	@include('layouts.common.head')
+	@yield('HeadAddOns')
+</head>
+<body>
+
+@yield('content')
+@include('layouts.common.foot')
+@yield('footAddOns')
+
+</body>
 </html>
