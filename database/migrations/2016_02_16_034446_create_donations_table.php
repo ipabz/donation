4d77b7ff8a5600bed03b4467c49251d795ad1621
Leo Neil <lnllaurito@yahoo.com>
2016-02-16 12:17:08 +0800

    Update 2016_02_16_034446_create_donations_table.php
    Added note field

--- database/migrations/2016_02_16_034446_create_donations_table.php
+++ database/migrations/2016_02_16_034446_create_donations_table.php
@@ -18,6 +18,7 @@ class CreateDonationsTable extends Migration
             $table->integer('credit_card_id');
             $table->integer('organization_id');
             $table->float('amount',10, 2);
+            $table->text('note')->nullable();
             $table->integer('recur')->default(0);
             $table->integer('frequency_id')->nullable();
             $table->timestamps();
